1265abbc21d049f6b7155af00bf8257a7ee91b22
ArslanMuhammadAliDawood <faisalarsalanali@gmail.com>
2025-01-28 18:37:05 +0000

    Add error handling for invalid user input

--- index.js
+++ index.js
@@ -20,35 +20,52 @@ app.use(express.static("public"));
 
 
 let total;
-
+let data;
 app.get("/", async (req, res) => {
-  let data;
-  const query = "SELECT country_code FROM visited_countries";
-  let results = await new Promise ((resolve, reject)=>{
-    db.query(query, (err, queryRes)=>{
-      if (err){
-        console.log("Error executing query", err.stack);
-        reject(err);
-      }
-      resolve(queryRes.rows);
-    });
-  });
-  data = visitedCountries(results)
-  total = results.length;
-  res.render("index.ejs", {countries: data, total: total})
+  const result = await fetchVistedCountries();
+  data = result.data;
+  total = result.total;
+  res.status(200).render("index.ejs", {countries: data, total: total})
 });
 
 app.post("/add", async(req, res)=>{
-  const country = req.body.country;
-  const query = "SELECT country_code FROM countries WHERE country_name = $1";
-  let queryResult = await db.query(query, [country]);
-  queryResult = queryResult.rows[0].country_code;
-  const insertQuery = "INSERT INTO visited_countries (country_code) VALUES ($1)";
-  await db.query(insertQuery, [queryResult]);
-  res.status(200).redirect("/");
+    const country = req.body.country;
+    let queryResult; 
+      try{
+        const query = "SELECT country_code FROM countries WHERE country_name = $1";
+        queryResult = await db.query(query, [country]);
+        queryResult = queryResult.rows[0].country_code;
+      }catch(err){
+        console.log(err);
+        const result = await fetchVistedCountries();
+        data = result.data;
+        total = result.total;
+        res.status(404).render("index.ejs", {total:total, countries:data, error:"Country does not exist. Try Again"});
+        return;
+      }
+      try{
+        const insertQuery = "INSERT INTO visited_countries (country_code) VALUES ($1)";
+        await db.query(insertQuery, [queryResult]);
+
+      }catch(err){
+        console.log(err);
+        const result = await fetchVistedCountries();
+        data = result.data;
+        total = result.total;
+        res.status(404).render("index.ejs", {total:total, countries:data, error:"Country already visited. Try Again"});
+        return;
+      }
+      res.status(200).redirect("/");
 });
 
-const visitedCountries = (data)=>{
+const fetchVistedCountries = async() =>{
+  const query = "SELECT country_code FROM visited_countries";
+  const results = await db.query(query);
+  data = visitedCountries(results.rows);
+  total = results.rows.length;
+  return { data, total };
+}
+  const visitedCountries = (data)=>{
   let countries="";
   for (let i=0; i< data.length; i++){
       if (i!=data.length-1){
